2115a8baa6ee6754ddf4e07837bb665f0428fad7
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-21 22:59:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,15 +52,15 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
+    sql = <<-SQL 
       SELECT *
       FROM students
       WHERE name = ?
-    SQL
+    SQL 
 
-    DB[:conn].execute(sql, name).map |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end
+    end 
   end
 
 
