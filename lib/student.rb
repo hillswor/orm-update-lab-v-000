ffb6fe594b6d7305370c088c948d5038fbea3a57
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-21 23:01:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,11 +56,11 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
-    SQL 
+    SQL
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end 
+    end
   end
 
 
